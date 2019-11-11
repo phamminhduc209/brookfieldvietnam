3cd88fb9c0e91c66ffc0a4b9d20ff2804339b9cb
Ducpm <minhducpham.it@gmail.com>
2019-11-12 06:19:46 +0700

    Code index

--- js/main.js
+++ js/main.js
@@ -11,3 +11,24 @@ var swiper = new Swiper('.swiper-container', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() >= 200) {
+    $('#return-to-top').addClass('td-scroll-up-visible');
+  } else {
+    $('#return-to-top').removeClass('td-scroll-up-visible');
+  }
+});
+$('#return-to-top').click(function() {
+  $('body,html').animate({
+    scrollTop : 0
+  }, 'slow');
+});
+
+$(document).ready(function(){
+  $(".header_menu").sticky({
+    topSpacing: 0,
+    zIndex: 99
+  });
+});
