6d88bc369606b5df5570a32b6f6824af9a5ec8ca
Ducpm <minhducpham.it@gmail.com>
2019-11-14 06:42:28 +0700

    Detail Page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-var swiper = new Swiper('.swiper-container', {
+(function($){
+  "use strict";
+
+  var swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
@@ -13,22 +16,39 @@ var swiper = new Swiper('.swiper-container', {
 });
 
 
-$(window).scroll(function() {
-  if ($(this).scrollTop() >= 200) {
-    $('#return-to-top').addClass('td-scroll-up-visible');
-  } else {
-    $('#return-to-top').removeClass('td-scroll-up-visible');
-  }
-});
-$('#return-to-top').click(function() {
-  $('body,html').animate({
-    scrollTop : 0
-  }, 'slow');
-});
-
-$(document).ready(function(){
-  $(".header_menu").sticky({
-    topSpacing: 0,
-    zIndex: 99
+  $(window).scroll(function() {
+    if ($(this).scrollTop() >= 200) {
+      $('#return-to-top').addClass('td-scroll-up-visible');
+    } else {
+      $('#return-to-top').removeClass('td-scroll-up-visible');
+    }
   });
-});
+  $('#return-to-top').click(function() {
+    $('body,html').animate({
+      scrollTop : 0
+    }, 'slow');
+  });
+
+  $(document).ready(function(){
+    $(".header_menu").sticky({
+      topSpacing: 0,
+      zIndex: 99
+    });
+  });
+
+  $(".bxslider").bxSlider({
+    pagerCustom: '.product_thumb_img',
+    nextText: '<i class=fa fa-rounded-right"></i>',
+    prevText: '<i class="fa fa-rounded-left"></i>'
+  });
+
+  $(".product_images_thumb").bxSlider({
+    slideMargin: 10,
+    maxSlides: 5,
+    pager: false,
+    controls: false,
+    slideWidth: 86,
+    infiniteLoop: false
+  });
+
+})(jQuery); // End of use strict
